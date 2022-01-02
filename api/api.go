50013f1ed1d1f33d08751854f26ce7bcb1cf411f
teddybear0430 <teddybear0430@hotmail.com>
2022-01-02 15:15:37 +0900

    feat: goroutineを使ってパフォーマンス改善。

--- api/api.go
+++ api/api.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // Marshalする際に構造体タグを利用することでMarshalの結果を一部制御できる
@@ -43,7 +44,15 @@ func GetHackerNews(n int) []HackerNews {
 	json.Unmarshal(body, &ids)
 
 	var hns []HackerNews
-	var hn HackerNews
+
+	// WaitGroupを宣言する
+	// WaitGroupを使用することで、すべてのgoroutineが終了した時点で次の処理に移ることができる
+	wg := new(sync.WaitGroup)
+
+	// channelの生成
+	// channel生成時にバッファの数を指定する
+	chn := make(chan HackerNews, len(ids))
+
 	count := 0
 
 	for _, v := range ids {
@@ -51,23 +60,45 @@ func GetHackerNews(n int) []HackerNews {
 			break
 		}
 
+		// 終了待ちするgoroutineの数を設定する
+		wg.Add(1)
+
 		url := fmt.Sprintf("https://hacker-news.firebaseio.com/v0/item/%s.json?print=pretty", strconv.Itoa(v))
-		res, _ := http.Get(url)
-		body, _ := ioutil.ReadAll(res.Body)
-		json.Unmarshal(body, &hn)
 
-		if hn.Url != "" {
-			description := getDescription(hn.Url)
+		var hn HackerNews
 
-			if description != "" {
-				hn.Description = getDescription(hn.Url)
+		// goroutineとしてリクエストを行う処理を呼び出す
+		go func(url string) {
+			res, _ := http.Get(url)
+			body, _ := ioutil.ReadAll(res.Body)
+			json.Unmarshal(body, &hn)
+
+			if hn.Url != "" {
+				description := getDescription(hn.Url)
+
+				if description != "" {
+					hn.Description = getDescription(hn.Url)
+				}
 			}
-		}
 
-		hns = append(hns, hn)
+			// チャネルに書き込みを行う
+			chn <- hn
+
+			// WaitGroupに設定された数だけDoneが実行されるまで待機
+			defer wg.Done()
+		}(url)
+
+		hns = append(hns, <-chn)
 
 		count += 1
 	}
+
+	// 【重要】データを送信し終わったらチャネルをcloseする
+	close(chn)
+
+	// 全ての処理が終わるまで待機
+	wg.Wait()
+	fmt.Println(hns)
 
 	// hacker newsの情報を含む構造体を返却する
 	return hns
